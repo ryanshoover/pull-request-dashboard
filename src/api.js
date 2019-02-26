92e9d7faf7fced78aecbefc6bc8d2aec33536992
Ryan Hoover <ryanshoover@gmail.com>
2019-02-26 00:06:17 -0600

    Fix promises

--- src/api.js
+++ src/api.js
@@ -1,5 +1,23 @@
 const request = require('request');
 
+function getPulls() {
+	return new Promise( ( resolve ) => {
+		getTeamRepos()
+		.then( repos => Promise.all( repos.map( getRepoPulls ) ) )
+		.then( repos => {
+			const allPulls = [];
+
+			repos.forEach( pulls => {
+				pulls.forEach( pull => allPulls.push( pull ) );
+			 } );
+
+			return processPulls( allPulls );
+		} )
+		.then( data => resolve( data ) )
+		.catch( err => console.error( 'getPulls catch', err ) );
+	} );
+}
+
 function getTeamRepos() {
 	const options = {
 		url: `https://api.github.com/teams/${ process.env.GITHUB_TEAM }/repos`,
@@ -24,26 +42,6 @@ function getTeamRepos() {
 			resolve( body );
 		} );
 	})
-}
-
-function getPulls() {
-	return new Promise( ( resolve ) => {
-		getTeamRepos()
-		.then( repos => {
-			return Promise.all( repos.map( getRepoPulls ) );
-		} )
-		.then( repos => {
-			const allPulls = [];
-
-			repos.forEach( pulls => {
-				pulls.forEach( pull => allPulls.push( pull ) );
-			 } );
-
-			return processPulls( allPulls );
-		} )
-		.then( data => resolve( data ) )
-		.catch( err => console.error( 'getPulls catch', err ) );
-	} );
 }
 
 function getRepoPulls( repo ) {
@@ -79,31 +77,26 @@ function processPulls( pulls ) {
 		owners: {},
 	};
 
-	const data = {
-		repos: {},
-		reviewers: {},
-		owners: {},
-	};
+	return new Promise( ( resolve ) => {
+		pulls.forEach( ( pull ) => {
+			pull.requested_reviewers.forEach( reviewer => {
+				counts.reviewers[ reviewer.login ] = counts.reviewers[ reviewer.login ] || 0;
+				counts.reviewers[ reviewer.login]++;
+			} );
 
-	pulls.forEach( ( pull ) => {
-		pull.requested_reviewers.forEach( reviewer => {
-			counts.reviewers[ reviewer.login ] = counts.reviewers[ reviewer.login ] || 0;
-			counts.reviewers[ reviewer.login]++;
+			const owner = pull.assignee || pull.user;
+			counts.owners[ owner.login ] = counts.owners[ owner.login ] || 0;
+			counts.owners[ owner.login ]++;
+
+			const repo = pull.url.match( /\/repos\/[^/]+\/([^/]+)\// )[1];
+			counts.repos[ repo ] = counts.repos[ repo ] || 0;
+			counts.repos[ repo ]++;
 		} );
 
-		const owner = pull.assignee || pull.user;
-		counts.owners[ owner.login ] = counts.owners[ owner.login ] || 0;
-		counts.owners[ owner.login ]++;
-
-		const repo = pull.url.match( /\/repos\/[^/]+\/([^/]+)\// )[1];
-		counts.repos[ repo ] = counts.repos[ repo ] || 0;
-		counts.repos[ repo ]++;
+		resolve( counts );
 	} );
-
-	return data;
 }
 
 module.exports = {
-	getTeamRepos,
 	getPulls,
 };
