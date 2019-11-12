215bf5d0d33a27bbd9a63053c19d173ee3ae8e09
Ryan Hoover <ryanshoover@gmail.com>
2019-11-12 14:19:20 -0600

    yarn upgrade

--- dist/static/js/bundle.js
+++ dist/static/js/bundle.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0961a5edb129ca2b7843";
+/******/ 	var hotCurrentHash = "310abf616d99e49bc442";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
